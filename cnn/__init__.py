b1ec9d1cb90aae6f6f7be04b4d8a6052331d1707
oszn <oszn>
2022-10-30 21:57:17 +0800

    add dynamic conv

---
+++ cnn/__init__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py   
+@Contact :   1091756452@qq.com
+@License :   (C)Copyright 2022-2024
+ 
+@Desciption: 
+
+@Author: oszn(y.liu)
+@Modify Time    
+------------ 
+2022/10/30 21:28       
+'''
+from . import convMoudle
+# from .convMoudle import *
+#
+# # __all__ = []
+#
+# __all__ = (convMoudle.__all__)
---
+++ cnn/convMoudle/__init__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py   
+@Contact :   1091756452@qq.com
+@License :   (C)Copyright 2022-2024
+ 
+@Desciption: 
+
+@Author: oszn(y.liu)
+@Modify Time    
+------------ 
+2022/10/30 21:28       
+'''
+
+from .cond_conv import CondConv
+from .dynamic_conv import DynamicConv
+
+__all__ = ['CondConv', 'DynamicConv']
